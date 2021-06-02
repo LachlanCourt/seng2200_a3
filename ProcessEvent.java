bb9ae31ae6098c127ef97e7706dc4a007af23037
LachlanCourt <lachlan.m.court@gmail.com>
2021-06-03 03:16:59 +1000

    Add comments to TimeEvent

--- ProcessEvent.java
+++ ProcessEvent.java
@@ -3,7 +3,7 @@
  ****    c3308061
  ****    Lachlan Court
  ****    03/06/2021
- ****    This class is a purely designed for reporting at the end of the production
+ ****    This class is purely designed for reporting at the end of the production
  ****    run. It stores information about an Item going through a single stage
  *******************************************************************************/
 public class ProcessEvent
--- TimeEvent.java
+++ TimeEvent.java
@@ -1,28 +1,42 @@
+/*******************************************************************************
+ ****    SENG2200 Assignment 3
+ ****    c3308061
+ ****    Lachlan Court
+ ****    03/06/2021
+ ****    This class is used to determine when a Stage is due to finish producing
+ ****    an Item. It implements Comarable so that it can be added to a PriorityQueue
+ *******************************************************************************/
 public class TimeEvent implements Comparable<TimeEvent>
 {
+    // Instance variable
     private double completionTime;
 
+    // Main Constructor
     public TimeEvent(double completionTime_)
     {
         completionTime = completionTime_;
     }
 
+    // Getter
     public double getCompletionTime()
     {
         return completionTime;
     }
 
+    /**
+     * Implement compareTo method
+     * @param o another TimeEvent
+     * @return a -1 or 1 to determine if the TimeEvent comes before or after o
+     */
     @Override
     public int compareTo(TimeEvent o)
     {
+        // Compare the times
         if (o.getCompletionTime() < this.completionTime)
         {
             return 1;
         }
-        else if (o.getCompletionTime() == this.completionTime)
-        {
-            return 0;
-        }
+        // If they are the same or o is > this
         return -1;
     }
 }
